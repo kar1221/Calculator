e083b581066d0427e96e0051a5849431f9dba4fe
kar1221 <khyeong191@gmail.com>
2024-02-23 09:55:12 +0800

    Finish the project
    some bugs are expected, will try to fix when I find one.

--- index.js
+++ index.js
@@ -1,18 +1,132 @@
-function add(numOne, numTwo) {
-  return numOne + numTwo;
-}
+let displayValue = "0";
+let actualValue = 0;
+let operator;
+let completeCalculation = false;
 
-function subtract(numOne, numTwo) {
-  return numOne - numTwo;
-}
-
-function multiply(numOne, numTwo) {
-  return numOne * numTwo;
-}
-
-function divide(numOne, numTwo) {
-  if (numTwo === 0) {
-    return "Cannot divide by zero";
+function onButtonClick(value) {
+  if (isNaN(value)) {
+    handleSymbol(value);
+  } else {
+    handleDigit(value);
   }
-  return numOne / numTwo;
+
+  updateDisplay(displayValue);
+}
+
+function handleSymbol(symbol) {
+  switch (symbol) {
+    case "AC":
+      displayValue = "0";
+      actualValue = 0;
+      operator = null;
+      break;
+    case "Del":
+      if (completeCalculation) {
+        displayValue = "0";
+        completeCalculation = false;
+        return;
+      }
+
+      if (displayValue.length === 1) {
+        displayValue = "0";
+      } else {
+        displayValue = displayValue.slice(0, -1);
+      }
+      break;
+    case "+":
+    case "-":
+    case "×":
+    case "÷":
+      handleOperator(symbol);
+      break;
+    case "=":
+      if (operator === null) {
+        return;
+      }
+      calculate(+displayValue);
+      displayValue = actualValue.toString();
+      actualValue = 0;
+      operator = null;
+      completeCalculation = true;
+      emptyOverview();
+      break;
+    case ".":
+      if (!displayValue.includes(".")) {
+        displayValue += ".";
+      }
+      break;
+  }
+}
+
+function handleOperator(symbol) {
+  if (actualValue === 0) {
+    actualValue = +displayValue;
+  } else {
+    calculate(+displayValue);
+  }
+
+  operator = symbol;
+  displayValue = "0";
+  updateOverview(actualValue, operator);
+}
+
+function calculate(value) {
+  switch (operator) {
+    case "+":
+      actualValue += value;
+      break;
+    case "-":
+      actualValue -= value;
+      break;
+    case "×":
+      actualValue *= value;
+      break;
+    case "÷":
+      actualValue /= value;
+      break;
+  }
+}
+
+function handleDigit(digit) {
+  if (completeCalculation) {
+    displayValue = digit;
+    completeCalculation = false;
+    return;
+  }
+
+  if (displayValue === "0") {
+    displayValue = digit;
+  } else {
+    displayValue += digit;
+  }
+}
+
+function updateOverview(value, symbol) {
+  document.getElementById("display-history").textContent = `${value} ${symbol}`;
+}
+
+function emptyOverview() {
+  document.getElementById("display-history").textContent = "";
+}
+
+function updateDisplay(value) {
+  const displayPanel = document.getElementById("display");
+  if (value === "Infinity" || value === "NaN") {
+    displayPanel.textContent = "ಠ_ಠ";
+    return;
+  }
+
+  if (value.length > 10) {
+    displayPanel.textContent = value.slice(0, 9) + "...";
+    return;
+  }
+
+  displayPanel.textContent = value;
+}
+
+const buttons = document.querySelectorAll(".key");
+for (const button of buttons) {
+  button.addEventListener("click", (event) => {
+    onButtonClick(event.target.textContent);
+  });
 }
